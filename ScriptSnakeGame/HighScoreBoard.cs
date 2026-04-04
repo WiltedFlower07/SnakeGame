8c5a7513e8085825a99a07152aa4b8ebf1b1c7f5
dannypark1201-gif <dannypark1201@gmail.com>
2026-04-04 07:38:01 +0700

    Update HighScoreBoard.cs

--- ScriptSnakeGame/HighScoreBoard.cs
+++ ScriptSnakeGame/HighScoreBoard.cs
@@ -14,7 +14,43 @@ public class HighScoreBoard : MonoBehaviour
 
     public void SaveScore()
     {
-        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreBoard : MonoBehaviour
+{
+    public Text scoreText;      // UI hiện điểm hiện tại
+    public Text highScoreText;  // UI hiện high score
+
+    private int score = 0;
+    private int highScore = 0;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateUI();
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        }
+
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+        highScoreText.text = "High Score: " + highScore;
+    }
+}
 
         if (currentScore > highScore)
         {
